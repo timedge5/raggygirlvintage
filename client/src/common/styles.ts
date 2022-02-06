98bda85ec2e50ce80db95e4a3506216f574fca81
Tim Edge <timedge5@gmail.com>
2022-02-06 11:27:58 -0800

    Added header links and styles for mobile/web

--- client/src/common/styles.ts
+++ client/src/common/styles.ts
@@ -1,3 +1,7 @@
 // Header
 
-export const headerButton = "material-icons text-slate-800 cursor-pointer p-2 rounded-full hover:bg-gray-100"
+export const headerButton =
+  "material-icons text-slate-800 cursor-pointer p-2 rounded-full hover:bg-gray-100";
+export const headerNavLink = "uppercase font-light px-2";
+export const headerNavLinkActive =
+  "uppercase font-regular px-2 py-1 border-b border-gray-900";
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,30 @@
-import { headerButton } from "../common/styles";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  headerButton,
+  headerNavLink,
+  headerNavLinkActive,
+} from "../common/styles";
+
+const links = ["home", "shop", "about", "contact"];
 
 export const Header = () => {
+  const [active, setActive] = useState("home");
   return (
-    <div className="w-full h-12 p-2 flex justify-between items-center bg-white drop-shadow-lg fixed">
-      <img src="/resources/main_logo.webp" className="h-8" />
-      <span className={headerButton}>menu</span>
+    <div className="w-full h-12 p-2 px-3 flex justify-between items-center bg-white drop-shadow-lg fixed md:px-10">
+      <img src="/resources/main_logo.webp" className="h-8" alt="RaggyGirlVintage"/>
+      <div className="justify-center items-center hidden md:flex">
+        {links.map((link: string) => (
+          <Link
+            to={link === "home" ? "/" : `/${link}`}
+            onClick={() => setActive(link)}
+            className={active === link ? headerNavLinkActive : headerNavLink}
+          >
+            {link}
+          </Link>
+        ))}
+      </div>
+      <span className={`${headerButton} md:hidden`}>menu</span>
       <div className="relative">
         <span className={headerButton}>shopping_cart</span>
         <div className="absolute cursor-pointer flex justify-center items-center bottom-0 right-0 text-white text-xs bg-red-400 h-4 w-4 rounded-full">
