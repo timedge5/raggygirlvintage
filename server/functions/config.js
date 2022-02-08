491562500f4e9b420c623efa9218bff5d4af9e75
Tim Edge <timedge5@gmail.com>
2022-02-07 22:21:03 -0800

    Set env vars for firebase, cleaned up files, backend done

--- server/functions/config.js
+++ server/functions/config.js
@@ -1,19 +1,20 @@
 const admin = require("firebase-admin");
+const functions = require("firebase-functions");
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    type: process.env.TYPE,
-    project_id: process.env.PROJECT_ID,
-    private_key_id: process.env.PRIVATE_KEY_ID,
-    private_key: process.env.PRIVATE_KEY,
-    client_email: process.env.CLIENT_EMAIL,
-    client_id: process.env.CLIENT_ID,
-    auth_uri: process.env.AUTH_URI,
-    token_uri: process.env.TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+    type: functions.config().project.type,
+    project_id: functions.config().project.project_id,
+    private_key_id: functions.config().project.private_key_id,
+    private_key: functions.config().project.private_key.replace(/\\n/g, "\n"),
+    client_email: functions.config().project.client_email,
+    client_id: functions.config().project.client_id,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: functions.config().project.client_x509_cert_url,
   }),
-  databaseURL: process.env.DB_URL,
+  databaseURL: functions.config().project.database_urls,
 });
 
 const db = admin.firestore();
